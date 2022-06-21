b26986dfb4ddec98dd64799a86588edaa6e5d1ef
Daniel Uboa <uboadaniel@gmail.com>
2022-06-21 09:46:14 +0300

    Add User model

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,4 +1,6 @@
+from email.policy import default
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Site(models.Model):
@@ -12,3 +14,21 @@ class Site(models.Model):
 
     def __str__(self):
         return self.name
+
+# New user model
+class User(AbstractUser):
+    avatar = models.ImageField(
+        upload_to='user/avatars/%Y/%m/%d/',
+        default='user/avatars/default.jpg',
+    )
+    bio = models.TextField(max_length=500, null=True)
+    location = models.CharField(max_length=30, null=True)
+    website = models.CharField(max_length=100, null=True)
+    joined_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = '2. Users'
+
+    def __str__(self):
+        return self.username
