08656a939450883d5635e9b8f625c2ffddd359b3
Daniel Uboa <uboadaniel@gmail.com>
2022-06-23 12:47:12 +0300

    Install and configure tailwindcss

--- frontend/blog/src/App.vue
+++ frontend/blog/src/App.vue
@@ -9,6 +9,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
