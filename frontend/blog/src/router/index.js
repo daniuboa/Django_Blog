e973d7fd085e1d703631f1b330cc5b1c365d30fc
Daniel Uboa <uboadaniel@gmail.com>
2022-06-23 13:15:41 +0300

    Create routes

--- frontend/blog/src/router/index.js
+++ frontend/blog/src/router/index.js
@@ -1,23 +1,47 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import PostView from "@/views/main/Post.vue";
+import CategpryView from "@/views/main/Category.vue";
+import TagView from "@/views/main/Tag.vue";
+import AllCategoriesView from "@/views/main/AllCategories.vue";
+import AllTagsView from "@/views/main/AllTags.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: HomeView,
+  },
+  {
+    path: "/category",
+    name: "Category",
+    component: CategoryView,
+  },
+  {
+    path: "/tag",
+    name: "Tag",
+    component: TagView,
+  },
+  {
+    path: "/post",
+    name: "Post",
+    component: Postview,
+  },
+  {
+    path: "/categories",
+    name: "Categories",
+    component: AllCategoriesView,
+  },
+  {
+    path: "/tags",
+    name: "Tags",
+    component: AllTagsView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  routes,
+});
 
-export default router
+export default router;
