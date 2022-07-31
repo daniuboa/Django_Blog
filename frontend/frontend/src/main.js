74b5f578ed62c7d33e31a2f423b0ed46c09d073c
Dani Uboa <uboadaniel@gmail.com>
2022-07-31 12:27:49 +0300

    Create mutations.js

--- frontend/frontend/src/main.js
+++ frontend/frontend/src/main.js
@@ -2,11 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./main.css";
+import { apolloProvider } from "@/apollo-config";
+import { createPinia } from "pinia";
 
-import "./assets/main.css";
-
-const app = createApp(App);
-
-app.use(router);
-
-app.mount("#app");
+createApp(App)
+  .use(createPinia())
+  .use(router)
+  .use(apolloProvider)
+  .mount("#app");
