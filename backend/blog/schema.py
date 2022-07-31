b0a0430a74678a63bedcdf94ef5ec955ba6df74e
Dani Uboa <uboadaniel@gmail.com>
2022-07-31 11:46:37 +0300

    Update schema.py

--- backend/blog/schema.py
+++ backend/blog/schema.py
@@ -1,19 +1,7 @@
 import graphene
-from graphene_django import DjangoObjectType
-from blog import models, queries
+from blog import queries, mutations
 
 # Define type
-class SiteType(DjangoObjectType):
-    class Meta:
-        model = models.Site
 
-# The Query class
-class Query(graphene.ObjectType):
-    site = graphene.Field(types.SiteType)
 
-    def resolve_site(root, info):
-        return (
-            models.Site.objects.first()
-        )
-
-schema = graphene.Schema(query=queries.Query)
+schema = graphene.Schema(query=queries.Query, mutation=mutations.Mutation)
