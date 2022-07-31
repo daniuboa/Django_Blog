6356f4379c80873dd6e7e508c93f42777fd87c9f
Dani Uboa <uboadaniel@gmail.com>
2022-07-31 12:04:45 +0300

    Update index.js

--- frontend/frontend/src/router/index.js
+++ frontend/frontend/src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 import HomeView from "@/views/main/Home.vue";
 import PostView from "@/views/main/Post.vue";
 import CategoryView from "@/views/main/Category.vue";
 import TagView from "@/views/main/Tag.vue";
 import AllCategoriesView from "@/views/main/AllCategories.vue";
 import AllTagsView from "@/views/main/AllTags.vue";
+import SignInView from "@/views/user/SignIn.vue";
+import SignUpView from "@/views/user/SignUp.vue";
+import ProfileView from "@/views/user/Profile.vue";
 
 const routes = [
   {
@@ -14,17 +16,17 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/category",
+    path: "/category/:category",
     name: "Category",
     component: CategoryView,
   },
   {
-    path: "/tag",
+    path: "/tag/:tag",
     name: "Tag",
     component: TagView,
   },
   {
-    path: "/post",
+    path: "/post/:slug",
     name: "Post",
     component: PostView,
   },
@@ -38,10 +40,25 @@ const routes = [
     name: "Tags",
     component: AllTagsView,
   },
+  {
+    path: "/signin",
+    name: "SignIn",
+    component: SignInView,
+  },
+  {
+    path: "/signup",
+    name: "SignUp",
+    component: SignUpView,
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: ProfileView,
+  },
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
