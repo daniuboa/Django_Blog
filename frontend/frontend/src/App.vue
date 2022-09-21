41739208aece90a47171bd43ebce5f01c9ed7c95
Dani Uboa <uboadaniel@gmail.com>
2022-09-21 14:40:49 +0300

    install vue

--- frontend/frontend/src/App.vue
+++ frontend/frontend/src/App.vue
@@ -9,7 +9,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-      <h1 class="text-3xl font-bold underline">Hello World</h1>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
