6dd187874da995385d3b48bdd874f059c1565542
Dani Uboa <uboadaniel@gmail.com>
2022-09-21 15:13:56 +0300

    creating routes with vue router

--- frontend/frontend/src/router/index.js
+++ frontend/frontend/src/router/index.js
@@ -1,23 +1,47 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import PostView from '../views/PostView.vue'
+import CategoryView from '../views/CategoryView.vue'
+import TagView from '../views/TagView.vue'
+import AllCategoriesView from '../views/AllCategoriesView.vue'
+import AllTagsView from '../views/AllTagsView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/category',
+    name: '/Category',
+    component: CategoryView,
+  },
+  {
+    path: '/tag',
+    name: '/Tag',
+    component: TagView,
+  },
+  {
+    path: '/post',
+    name: '/Post',
+    component: PostView,
+  },
+  {
+    path: 'categories',
+    name: 'Categories',
+    components: AllCategoriesView,
+  },
+  {
+    path: 'tags',
+    name: 'Tags',
+    components: AllTagsView,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes,
 })
 
 export default router
